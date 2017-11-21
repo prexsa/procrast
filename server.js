36a2c95051d73ea23536800ad8b0f134fd10f138
prexsa <prexgit@gmail.com>
2017-11-21 13:21:34 -0800

    inital commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const port = (process.env.PORT || 8080)
+const app = express()
+
+const indexPath = path.join(__dirname, 'index.html')
+const publicPath = express.static(path.join(__dirname, 'public'))
+
+app.use('/public', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+
+  module: {
+    rules: [
+      { test: /\.js?$/,
+        loader: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.scss?$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        include: path.join(__dirname, 'src', 'styles') 
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
